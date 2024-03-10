94730026f3c25d2a88c92a85f7e92f0caaf76847
beeplovsharma <beeplovsharma@gmail.com>
2024-03-11 01:31:17 +0530

    fetched api for live bitcoin updates

--- src/app/components/HeroSection/Hero.js
+++ src/app/components/HeroSection/Hero.js
@@ -1,8 +1,10 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import PriceChart from "../PriceChart/PriceChart";
 import GetStarted from "../GetStarted/GetStarted";
 import TrendingCoins from "../TrendingCoins/TrendingCoins";
 import Tokenomics from "../Tokenomics/Tokenomics";
+import { MdOutlineArrowDropUp } from "react-icons/md";
 import Team from "../Team/Team";
 import About from "../About/About";
 import Sentiment from "../Sentiment/Sentiment";
@@ -10,6 +12,34 @@ import Performance from "../Performance/Performance";
 import Image from "next/image";
 
 function Hero() {
+  const [data, setData] = useState({
+    inr: 5742621,
+    usd: 69395,
+    perc: 2.51,
+  });
+  const fetchData = async () => {
+    try {
+      const raw = await fetch(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr%2Cusd&include_24hr_change=true"
+      );
+      if (raw.ok) {
+        const res = await raw.json();
+        setData({
+          inr: res.bitcoin.inr,
+          usd: res.bitcoin.usd,
+          perc: res.bitcoin.usd_24h_change,
+        });
+      } else {
+        console.error("Failed to fetch");
+      }
+    } catch (error) {
+      console.log("Error during fetching", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="hero w-full bg-[#eff2f5]">
@@ -24,6 +54,19 @@ function Hero() {
                   Rank #1
                 </button>
               </div>
+
+              <div className="flex flex-col gap-2">
+                <div className="flex items-center gap-4">
+                  <h1 className="font-bold text-4xl">${data.usd}</h1>
+                  <button className="text-[#2fb988] bg-[#ebf9f4] text-sm font-bold px-2 py-[2px] flex justify-center items-center">
+                    <MdOutlineArrowDropUp size={25} />
+                    {parseFloat(data.perc).toFixed(2)}%
+                  </button>
+                  <p className="opacity-60">(24h)</p>
+                </div>
+                <p className="font-semibold pb-4">₹{data.inr}</p>
+              </div>
+
               <div className="h-[350px] font-bold lg:h-[250px]">
                 <PriceChart />
               </div>
